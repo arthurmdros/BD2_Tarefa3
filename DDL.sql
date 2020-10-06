ff45d453f9f39fbcab5885ad1fa90946d3eb4656
Arthur Medeiros <53915502+arthurmdros@users.noreply.github.com>
2020-10-06 16:32:14 -0300

    Corrigindo DDL de criação

--- DDL.sql
+++ DDL.sql
@@ -3,6 +3,13 @@ CREATE TABLE atividade (
 	descricao VARCHAR(140)
 ) ENGINE=INNODB;
 
+CREATE TABLE departamento (
+	codigo INT,
+	descricao VARCHAR(140),
+	codGerente INT,
+   PRIMARY KEY(codigo)
+) ENGINE=INNODB;
+
 CREATE TABLE funcionario (
 	codigo INT,
 	nome VARCHAR(140),
@@ -17,24 +24,17 @@ CREATE TABLE funcionario (
 ) ENGINE=INNODB;
 
 
-CREATE TABLE departamento (
-	codigo INT,
-	descricao VARCHAR(140),
-   PRIMARY KEY(codigo)
-) ENGINE=INNODB;
-
 CREATE TABLE projeto (
 	codigo INT,
 	descricao VARCHAR(140),
-	codFuncionarioResponsavel INT,
+	codResponsavel INT,
 	dataInicio VARCHAR(140),
 	dataFim VARCHAR(140),
 	codDepto INT,
   	FOREIGN KEY (codDepto)
    REFERENCES departamento(codigo)
    ON UPDATE CASCADE ON DELETE RESTRICT,
-   CodFuncionarioResponsavel INT,
-  	FOREIGN KEY (CodFuncionarioResponsavel)
+  	FOREIGN KEY (codResponsavel)
    REFERENCES funcionario(codigo)
    ON UPDATE CASCADE ON DELETE RESTRICT,
    PRIMARY KEY(codigo)
@@ -44,6 +44,7 @@ CREATE TABLE AtividadeProjeto (
 	codigo INT,
 	codProjeto INT,
 	codAtividade INT,
+	codResponsavel INT,
 	dataInicio VARCHAR(140),
 	dataFim VARCHAR(140),
   	FOREIGN KEY (codProjeto)
@@ -52,10 +53,13 @@ CREATE TABLE AtividadeProjeto (
   	FOREIGN KEY (codAtividade)
    REFERENCES atividade(codigo)
    ON UPDATE CASCADE ON DELETE RESTRICT,
+   FOREIGN KEY (codResponsavel)
+   REFERENCES funcionario(codigo)
+   ON UPDATE CASCADE ON DELETE RESTRICT,
    PRIMARY KEY(codigo)
 ) ENGINE=INNODB;
 
 ALTER TABLE departamento
-ADD FOREIGN KEY (codFuncionarioGerente)
+ADD FOREIGN KEY (codGerente)
 REFERENCES funcionario(codigo)
 ON UPDATE CASCADE ON DELETE RESTRICT;
